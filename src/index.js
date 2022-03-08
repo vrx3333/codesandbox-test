c6d0802fdeef129eaab24edb0440f110e356e4c5
vrx3333 <101187128+vrx3333@users.noreply.github.com>
2022-03-08 22:58:21 +0900

    スプレッド構文まで

--- src/index.js
+++ src/index.js
@@ -1,10 +1,165 @@
-import "./styles.css";
+/**
+ * const let
+ **/
+// var val1 = "var変数";
+// console.log(val1);
+// var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書き可能
+// val2 = "letを上書き";
+// console.log(val2);
+
+// letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// const
+// val3 = "const変数を上書き";
+
+// const val3 = "const変数を再宣言";
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "test",
+//   age: 28
+// };
+// val4.name = "テスト";
+// val4.address = "Hiroshima";
+// console.log(val4);
+
+// constで定義したプロパティの変更は可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレートの文字列
+ */
+// const name = "ジャケ";
+// const age = 28;
+
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// // アロー関数
+// // const func2 = (str) => {
+// //   return str;
+// // };
+// const func2 = (str) => str;
+// console.log(func2("func2です。"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "ジャケ",
+//   age: 28
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProfile = ["ジャケ", 28];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん`);
+// sayHello(); // ↑ 引数の中に=〇〇で宣言されている場合、初期値が代入されている。
+// sayHello("ジャケ");
+
+/**
+ * スプレッド構文 ...
+ * 「...」がついていたら、順番に処理をする
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// const arr2 = [1, 2, 3, 4, 5];
+// const sumFunc2 = (num1, num2, num3, num4, num5) =>
+//   console.log(num1 + num2 + num3 + num4 + num5);
+// sumFunc2(...arr2);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// const [...arr4] = arr2;
+// console.log(arr4);
+
+/**
+ * 配列のコピー,結合
+ */
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4); // スプレッド構文 ...を使うと参照が引き継がれない
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4); // = で入れてしまうと元のarr4も値が変わってしまうため、...を使った方が良い
+
+/**
+ * mapやfilterを使った配列の処理
+ */
